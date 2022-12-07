236ddfc06a8616425e8d2209e44ec45f279f4cc8
bogomilJelev <76732542+bogomilJelev@users.noreply.github.com>
2022-12-07 14:17:31 +0200

    make solution

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace new_one.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
