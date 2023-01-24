a68765a582388e0d45bf397935017cef9f933fb2
bogomilJelev <bogomilljelev@gmail.com>
2023-01-24 14:17:51 +0200

    AddContragents

--- Warehouse.Infrastructure/Data/Category.cs
+++ Warehouse.Infrastructure/Data/Category.cs
@@ -9,18 +9,26 @@ namespace Warehouse.Infrastructure.Data
         [Key]
         public Guid Id { get; set; }= Guid.NewGuid();
 
+
         [Required]
         [StringLength(100)]
         public string Label { get; set; }
+
 
         [StringLength(500)]
         public string? Description { get; set; }
 
         [Required]
         [Column(TypeName = "date")]
+
+
         public DateTime DateFrom { get; set; } = DateTime.Today;
+
+
         [Column(TypeName = "date")]
         public DateTime? DateTo { get; set; }
+
+
         public IList<Item> Items { get; set; }= new List<Item>();
 
 
